6630033e26f2320d2d6272cab34433d642617c7f
IAmACodeGeek <copewithcode@gmail.com>
2024-07-31 21:09:52 +0530

    Created tools, Created a hierarchy of game objects
    Created the tools package which contains Vector(x, y) and Collider(for collision detection), Created the gameObject package which contains Entity(base interface), GameObject(has a position), Visible(a visible GameObject with an associated image)

---
+++ src/gameObject/Visible.java
@@ -0,0 +1,64 @@
+package gameObject;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+import com.tools.physics.Vector;
+
+public abstract class Visible extends GameObject implements Entity{
+	protected ImageIcon icon;
+	protected Image image;
+	protected boolean isDestroyed = false;
+	private Vector<Integer> imgSize;
+	
+	public Visible(String url, int width, int height) {
+		super(width, height);
+		imgSize = new Vector<>(width, height);
+		setImage(url, width, height);
+	}
+	
+	public Visible(String url, int width, int height, int x, int y) {
+		this(url, width, height);
+		this.setPosition(x, y);
+	}
+	
+	public int getImgWidth() {
+		return this.imgSize.x;
+	}
+	
+	public int getImgHeight() {
+		return this.imgSize.y;
+	}
+	
+	public void setImageSize(int imgWidth, int imgHeight) {
+		this.imgSize.set(imgWidth, imgHeight);
+		this.image = icon.getImage().getScaledInstance(imgWidth, imgHeight, 0);
+	}
+
+	public void setImage(String url, int imgWidth, int imgHeight) {
+		this.icon = new ImageIcon(url);
+		this.setImageSize(imgWidth, imgHeight);	
+	}
+	
+	public void setImage(Image image) {
+		this.image = image;
+		this.imgSize.set(image.getWidth(null), image.getHeight(null));
+	}
+	
+	public Image getImage() {
+		return this.image;
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		icon = null;
+		image = null;
+		imgSize = null;
+	}
+	
+	public abstract void update();
+	public abstract void draw(Graphics g);
+}
--- src/gameState/GameState.java
+++ src/gameState/GameState.java
@@ -3,7 +3,9 @@ package gameState;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
-public interface GameState {
+import gameObject.Entity;
+
+public interface GameState extends Entity{
 	void update();
 	void draw(Graphics g);
 	void keyPressed(int keyCode);
